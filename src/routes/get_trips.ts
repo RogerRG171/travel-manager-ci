d2e4a0b818bd04dee90247ad9f1423306a2af11d
RogerGN <roger.goulart.nascimento@gmail.com>
2024-07-25 17:52:37 -0300

    @feat: create route get trip

---
+++ src/routes/get_trips.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+
+export async function getTrips(app: FastifyInstance) {
+  app.get('/trips', async (request, reply) => {
+    const trips = await prisma.trip.findMany()
+    reply.status(200).send(trips)
+  })
+}
