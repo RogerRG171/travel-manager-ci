09e77cb77efa1104695ac83a3106d20217099f1c
RogerGN <roger.goulart.nascimento@gmail.com>
2024-07-25 17:51:54 -0300

    @resource: migrations

---
+++ prisma/migrations/20240724202544_add_on_delete_cascade/migration.sql
@@ -0,0 +1,17 @@
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Participant" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "is_confirmed" BOOLEAN NOT NULL DEFAULT false,
+    "is_owner" BOOLEAN NOT NULL DEFAULT false,
+    "trip_id" TEXT NOT NULL,
+    CONSTRAINT "Participant_trip_id_fkey" FOREIGN KEY ("trip_id") REFERENCES "Trip" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_Participant" ("email", "id", "is_confirmed", "is_owner", "name", "trip_id") SELECT "email", "id", "is_confirmed", "is_owner", "name", "trip_id" FROM "Participant";
+DROP TABLE "Participant";
+ALTER TABLE "new_Participant" RENAME TO "Participant";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
