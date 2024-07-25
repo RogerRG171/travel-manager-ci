348eaf181936ea1665ebde7628a397d587618195
RogerGN <roger.goulart.nascimento@gmail.com>
2024-07-25 17:51:17 -0300

    @feat: create route to register trip

---
+++ src/routes/create-trip.ts
@@ -0,0 +1,117 @@
+/* eslint-disable camelcase */
+import { FastifyInstance } from 'fastify'
+import { prisma } from '../lib/prisma'
+import dayjs from 'dayjs'
+import localizedFormat from 'dayjs/plugin/localizedFormat'
+import { z } from 'zod'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { getMailClient } from '../lib/mail'
+import nodemailer from 'nodemailer'
+import 'dayjs/locale/pt-br'
+
+dayjs.locale('pt-br')
+dayjs.extend(localizedFormat)
+
+export async function createTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    '/trips',
+    {
+      schema: {
+        body: z.object({
+          destination: z
+            .string()
+            .min(3, 'Minimum 3 characters')
+            .max(150, 'Maximum 150 characters'),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
+          owner_name: z
+            .string()
+            .min(3, 'Minimum 3 characters')
+            .max(150, 'Maximum 150 characters'),
+          owner_email: z.string().email({ message: 'Invalid email' }),
+          emails_to_invite: z.array(
+            z.string().email({ message: 'Invalid email' }),
+          ),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const {
+        destination,
+        starts_at,
+        ends_at,
+        owner_name,
+        owner_email,
+        emails_to_invite,
+      } = request.body
+
+      if (dayjs(starts_at).isBefore(new Date())) {
+        throw new Error('Invalid trip start date')
+      }
+
+      if (dayjs(ends_at).isBefore(dayjs(starts_at))) {
+        throw new Error('Invalid trip end date')
+      }
+
+      const trip = await prisma.trip.create({
+        data: {
+          destination,
+          starts_at,
+          ends_at,
+          participant: {
+            createMany: {
+              data: [
+                {
+                  name: owner_name,
+                  email: owner_email,
+                  is_owner: true,
+                  is_confirmed: true,
+                },
+
+                ...emails_to_invite.map((email) => ({
+                  email,
+                })),
+              ],
+            },
+          },
+        },
+      })
+
+      const formattedStartData = dayjs(starts_at).format('DD [de] MMMM')
+      const formattedEndData = dayjs(ends_at).format('LL')
+
+      const confirmTripLink = `http://localhost:3333/trips/${trip.id}/confirm`
+
+      const mail = await getMailClient()
+
+      const message = await mail.sendMail({
+        from: {
+          name: 'Equipe plann.er',
+          address: 'example@example.com',
+        },
+        to: {
+          name: owner_name,
+          address: owner_email,
+        },
+        subject: `Confirme sua viagem para ${destination} em ${formattedStartData}`,
+        html: `
+        <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+          <p>Você solicitou a criação de uma viagem para <strong>${destination}</strong> nas datas de <strong>${formattedStartData}</strong> a <strong>${formattedEndData}</strong>.</p>
+          <br>
+          <p>Para confirmar sua viagem, clique no link abaixo:</p>
+          <br>
+          <p>
+              <a href="${confirmTripLink}">Confirmar viagem</a>
+          </p>   
+          <br> 
+          <p>Caso você não saiba do que se trata esse e-mail, apenas ignore esse e-mail.</p>
+        </div>
+        `.trim(),
+      })
+
+      console.log(nodemailer.getTestMessageUrl(message))
+
+      return reply.status(201).send({ tripId: trip.id })
+    },
+  )
+}
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,26 @@
 import fastify from 'fastify'
 import dotenv from 'dotenv'
+import { createTrip } from './routes/create-trip'
+import { getTrips } from './routes/get_trips'
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
 
 const app = fastify()
 
+// fastify-type-provider-zod
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+// dotenv
 dotenv.config({ path: '.env' })
 const PORT = parseInt(process.env.PORT as string)
 const HOST = process.env.HOST as string
+
+// routes
+app.register(createTrip)
+app.register(getTrips)
 
 app.listen({ port: PORT, host: HOST }).then(() => {
   console.log(`HTTP server running on http://${HOST}:${PORT}`)
