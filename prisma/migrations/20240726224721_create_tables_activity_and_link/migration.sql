ec5d5ff9481adc657e50d68a9ee14b800e530d8d
RogerGN <roger.goulart.nascimento@gmail.com>
2024-07-26 19:51:24 -0300

    @feat: create models activity and link

---
+++ prisma/migrations/20240726224721_create_tables_activity_and_link/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Activity" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "occurs_at" DATETIME NOT NULL,
+    "trip_id" TEXT NOT NULL,
+    CONSTRAINT "Activity_trip_id_fkey" FOREIGN KEY ("trip_id") REFERENCES "Trip" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Link" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "trip_id" TEXT NOT NULL,
+    CONSTRAINT "Link_trip_id_fkey" FOREIGN KEY ("trip_id") REFERENCES "Trip" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
