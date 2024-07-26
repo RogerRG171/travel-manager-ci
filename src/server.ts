b1d73f1b22c36acb05a0ed8ad3bbf5dadcda7752
RogerGN <roger.goulart.nascimento@gmail.com>
2024-07-26 15:58:43 -0300

    @resource: install cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import fastify from 'fastify'
+import cors from '@fastify/cors'
 import dotenv from 'dotenv'
 import { createTrip } from './routes/create-trip'
 import { getTrips } from './routes/get_trips'
@@ -6,8 +7,14 @@ import {
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
+import { confirmTrip } from './routes/confirm-trip'
 
 const app = fastify()
+
+// cors
+app.register(cors, {
+  origin: true,
+})
 
 // fastify-type-provider-zod
 app.setValidatorCompiler(validatorCompiler)
@@ -21,6 +28,7 @@ const HOST = process.env.HOST as string
 // routes
 app.register(createTrip)
 app.register(getTrips)
+app.register(confirmTrip)
 
 app.listen({ port: PORT, host: HOST }).then(() => {
   console.log(`HTTP server running on http://${HOST}:${PORT}`)
