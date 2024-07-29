3716388e6c2b227e7000bd9a90e79b513853ef15
RogerGN <roger.goulart.nascimento@gmail.com>
2024-07-29 16:33:07 -0300

    @feat: order activities by date

--- src/routes/get-activities.ts
+++ src/routes/get-activities.ts
@@ -3,6 +3,7 @@ import { FastifyInstance } from 'fastify'
 import { prisma } from '../lib/prisma'
 import { z } from 'zod'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import { dayjs } from '../lib/dayjs'
 
 export async function getActivities(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
@@ -22,15 +23,34 @@ export async function getActivities(app: FastifyInstance) {
           id: tripId,
         },
         include: {
-          activity: true,
+          activity: {
+            orderBy: {
+              occurs_at: 'asc',
+            },
+          },
         },
       })
 
       if (!trip) {
         throw new Error('Trip not found.')
       }
+      const differenceInDaysBetweenTripStartsAndEnds = dayjs(trip.ends_at).diff(
+        trip.starts_at,
+        'days',
+      )
+      const activities = Array.from({
+        length: differenceInDaysBetweenTripStartsAndEnds + 1,
+      }).map((_, index) => {
+        const date = dayjs(trip.starts_at).add(index, 'day')
+        return {
+          date: date.toDate(),
+          activities: trip.activity.filter((activity) => {
+            return dayjs(activity.occurs_at).isSame(date, 'day')
+          }),
+        }
+      })
 
-      return reply.status(201).send({ activities: trip.activity })
+      return reply.status(201).send({ activities })
     },
   )
 }
